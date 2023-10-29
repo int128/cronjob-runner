6464c3218083282fa7a9ea9cba33bbcfc216d03f
Hidetake Iwata <int128@gmail.com>
2023-10-29 10:17:43 +0900

    Notify container state changes

--- pod.go
+++ pod.go
@@ -45,26 +45,34 @@ func (h *podEventHandler) OnAdd(obj interface{}, _ bool) {
 func (h *podEventHandler) OnUpdate(oldObj, newObj interface{}) {
 	oldPod := oldObj.(*corev1.Pod)
 	newPod := newObj.(*corev1.Pod)
+	h.notifyPodStatusChange(oldPod, newPod)
+	h.notifyContainerStatusChanges(newPod.Namespace, newPod.Name, oldPod.Status.InitContainerStatuses, newPod.Status.InitContainerStatuses)
+	h.notifyContainerStatusChanges(newPod.Namespace, newPod.Name, oldPod.Status.ContainerStatuses, newPod.Status.ContainerStatuses)
+}
+
+func (h *podEventHandler) notifyPodStatusChange(oldPod, newPod *corev1.Pod) {
 	if oldPod.Status.Phase == newPod.Status.Phase {
 		return
 	}
 	log.Printf("Pod %s/%s is %s", newPod.Namespace, newPod.Name, newPod.Status.Phase)
+}
 
-	for _, containerStatus := range newPod.Status.ContainerStatuses {
+func (h *podEventHandler) notifyContainerStatusChanges(namespace, podName string, oldStatuses, newStatuses []corev1.ContainerStatus) {
+	changedContainerStatuses := computeContainerStateChanges(oldStatuses, newStatuses)
+	for _, containerStatus := range changedContainerStatuses {
 		if containerStatus.State.Waiting != nil {
 			waiting := containerStatus.State.Waiting
 			log.Printf("Pod %s/%s: Container %s is waiting %s",
-				newPod.Namespace, newPod.Name, containerStatus.Name,
+				namespace, podName, containerStatus.Name,
 				formatContainerStatusMessage(waiting.Reason, waiting.Message))
 		}
 		if containerStatus.State.Running != nil {
-			log.Printf("Pod %s/%s: Container %s is running",
-				newPod.Namespace, newPod.Name, containerStatus.Name)
+			log.Printf("Pod %s/%s: Container %s is running", namespace, podName, containerStatus.Name)
 		}
 		if containerStatus.State.Terminated != nil {
 			terminated := containerStatus.State.Terminated
 			log.Printf("Pod %s/%s: Container %s is terminated with exit code %d %s",
-				newPod.Namespace, newPod.Name, containerStatus.Name, terminated.ExitCode,
+				namespace, podName, containerStatus.Name, terminated.ExitCode,
 				formatContainerStatusMessage(terminated.Reason, terminated.Message))
 		}
 	}
@@ -78,6 +86,41 @@ func formatContainerStatusMessage(reason, message string) string {
 		return fmt.Sprintf("(%s)", reason)
 	}
 	return fmt.Sprintf("(%s, %s)", reason, message)
+}
+
+func computeContainerStateChanges(oldStatuses, newStatuses []corev1.ContainerStatus) []corev1.ContainerStatus {
+	var changed []corev1.ContainerStatus
+	oldMap := mapContainerStatusByName(oldStatuses)
+	newMap := mapContainerStatusByName(newStatuses)
+	for containerName := range newMap {
+		oldState := getContainerState(oldMap[containerName])
+		newState := getContainerState(newMap[containerName])
+		if oldState != newState {
+			changed = append(changed, newMap[containerName])
+		}
+	}
+	return changed
+}
+
+func mapContainerStatusByName(containerStatuses []corev1.ContainerStatus) map[string]corev1.ContainerStatus {
+	containerStatusMap := make(map[string]corev1.ContainerStatus)
+	for _, containerStatus := range containerStatuses {
+		containerStatusMap[containerStatus.Name] = containerStatus
+	}
+	return containerStatusMap
+}
+
+func getContainerState(containerStatus corev1.ContainerStatus) string {
+	if containerStatus.State.Waiting != nil {
+		return "Waiting"
+	}
+	if containerStatus.State.Running != nil {
+		return "Running"
+	}
+	if containerStatus.State.Terminated != nil {
+		return "Terminated"
+	}
+	return ""
 }
 
 func (h *podEventHandler) OnDelete(obj interface{}) {
